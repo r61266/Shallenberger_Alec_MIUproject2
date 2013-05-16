b2e66d9691c5a689453e0e62d43b48b4d71893ed
r61266 <lightking@fullsail.edu>
2013-05-16 00:20:02 -0400

    Adding files to repo.

---
+++ json/json.js
@@ -0,0 +1,189 @@
+// JavaScript Document
+// Alec Shallenberger
+// May 1, 2013
+// Term 1304
+// VFW Project4
+
+var json = {
+	"checklist1": {
+		"holigroup": ["Holiday:", "Christmas"],
+		"app": ["Appitizer:", "Quesadilla"],
+		"mname": ["Main Meal:", "Ham"],
+		"side": ["Side Dishes:", "Macaroni and cheese, Salad"],
+		"date": ["Date Updated:", "2013-28-04"],
+		"amofpeople": ["Amount of People:", "16"],
+		"notes": ["Notes:", "Don't forget Salsa."]
+	},
+	"checklist2": {
+		"holigroup": ["Holiday:", "Thanksgiving"],
+		"app": ["Appitizer:", "Chips"],
+		"mname": ["Main Meal:", "Turkey"],
+		"side": ["Side Dishes:", "Casserole"],
+		"date": ["Date Updated:", "2012-04-07"],
+		"amofpeople": ["Amount of People:", "27"],
+		"notes": ["Notes:", "Dip for chips."]
+	},
+	"checklist3": {
+		"holigroup": ["Holiday:", "Easter"],
+		"app": ["Appitizer:", "Bread"],
+		"mname": ["Main Meal:", "Quesadilla"],
+		"side": ["Side Dishes:", "Mashed Potatoes"],
+		"date": ["Date Updated:", "2011-02-08"],
+		"amofpeople": ["Amount of People:", "24"],
+		"notes": ["Notes:", "Don't forget to bake cookies."]
+	},
+	"checklist4": {
+		"holigroup": ["Holiday:", "Thanksgiving"],
+		"app": ["Appitizer:", "Chips"],
+		"mname": ["Main Meal:", "Ham"],
+		"side": ["Side Dishes:", "Macaroni and cheese, Salad"],
+		"date": ["Date Updated:", "2013-28-04"],
+		"amofpeople": ["Amount of People:", "16"],
+		"notes": ["Notes:", "Don't forget Salsa."]
+	},
+	"checklist5": {
+		"holigroup": ["Holiday:", "Hanukkah"],
+		"app": ["Appitizer:", "Quesadilla"],
+		"mname": ["Main Meal:", "Ham"],
+		"side": ["Side Dishes:", "Macaroni and cheese, Salad"],
+		"date": ["Date Updated:", "2013-28-04"],
+		"amofpeople": ["Amount of People:", "16"],
+		"notes": ["Notes:", "Don't forget Salsa."]
+	},
+	"checklist6": {
+		"holigroup": ["Holiday:", "New Years"],
+		"app": ["Appitizer:", "Yams"],
+		"mname": ["Main Meal:", "Ham"],
+		"side": ["Side Dishes:", "Salad"],
+		"date": ["Date Updated:", "2011-07-04"],
+		"amofpeople": ["Amount of People:", "28"],
+		"notes": ["Notes:", "Cant't wait for fireworks."]
+	},
+	"checklist7": {
+		"holigroup": ["Holiday:", "Christmas"],
+		"app": ["Appitizer:", "None"],
+		"mname": ["Main Meal:", "Ham"],
+		"side": ["Side Dishes:", "Macaroni and cheese"],
+		"date": ["Date Updated:", "2013-28-04"],
+		"amofpeople": ["Amount of People:", "30"],
+		"notes": ["Notes:", "Presents, YEA!!."]
+	},
+	"checklist8": {
+		"holigroup": ["Holiday:", "Easter"],
+		"app": ["Appitizer:", "Eggs"],
+		"mname": ["Main Meal:", "Ham"],
+		"side": ["Side Dishes:", "Mashed Potatoes"],
+		"date": ["Date Updated:", "2006-22-04"],
+		"amofpeople": ["Amount of People:", "16"],
+		"notes": ["Notes:", "Time to find Eggs."]
+	},
+	"checklist9": {
+		"holigroup": ["Holiday:", "Thanksgiving"],
+		"app": ["Appitizer:", "Quesadilla"],
+		"mname": ["Main Meal:", "Turkey"],
+		"side": ["Side Dishes:", "Salad"],
+		"date": ["Date Updated:", "2013-28-04"],
+		"amofpeople": ["Amount of People:", "22"],
+		"notes": ["Notes:", "Turkey Time."]
+	},
+	"checklist10": {
+		"holigroup": ["Holiday:", "Christmas"],
+		"app": ["Appitizer:", "Salami slices"],
+		"mname": ["Main Meal:", "Ham"],
+		"side": ["Side Dishes:", "Macaroni and cheese, Salad"],
+		"date": ["Date Updated:", "2013-28-04"],
+		"amofpeople": ["Amount of People:", "19"],
+		"notes": ["Notes:", "Parent's are coming down from New York."]
+	},
+	"checklist11": {
+		"holigroup": ["Holiday:", "New Years"],
+		"app": ["Appitizer:", "none"],
+		"mname": ["Main Meal:", "Hamburgers"],
+		"side": ["Side Dishes:", "Macaroni and cheese, Salad"],
+		"date": ["Date Updated:", "2004-04-04"],
+		"amofpeople": ["Amount of People:", "5"],
+		"notes": ["Notes:", "Fireworks."]
+	},
+	"checklist12": {
+		"holigroup": ["Holiday:", "Fourth of July"],
+		"app": ["Appitizer:", "chips"],
+		"mname": ["Main Meal:", "Hotdogs"],
+		"side": ["Side Dishes:", "Casserole"],
+		"date": ["Date Updated:", "2008-22-03"],
+		"amofpeople": ["Amount of People:", "8"],
+		"notes": ["Notes:", "Don't forget to make bean dip."]
+	},
+	"checklist13": {
+		"holigroup": ["Holiday:", "Christmas"],
+		"app": ["Appitizer:", "Quesadilla"],
+		"mname": ["Main Meal:", "Hamburgers"],
+		"side": ["Side Dishes:", "Macaroni and cheese, Salad"],
+		"date": ["Date Updated:", "2013-05-04"],
+		"amofpeople": ["Amount of People:", "27"],
+		"notes": ["Notes:", "Busy day today."]
+	},
+	"checklist14": {
+		"holigroup": ["Holiday:", "Easter"],
+		"app": ["Appitizer:", "Quesadilla"],
+		"mname": ["Main Meal:", "Ham"],
+		"side": ["Side Dishes:", "Mashed Potatoes"],
+		"date": ["Date Updated:", "2011-25-12"],
+		"amofpeople": ["Amount of People:", "14"],
+		"notes": ["Notes:", "Make enough food for everyone."]
+	},
+	"checklist15": {
+		"holigroup": ["Holiday:", "St.Patricks"],
+		"app": ["Appitizer:", "none"],
+		"mname": ["Main Meal:", "Hamburgers"],
+		"side": ["Side Dishes:", "Macaroni and cheese, Salad"],
+		"date": ["Date Updated:", "2013-28-04"],
+		"amofpeople": ["Amount of People:", "16"],
+		"notes": ["Notes:", "Don't forget the Beer."]
+	},
+	"checklist16": {
+		"holigroup": ["Holiday:", "Fourth of July"],
+		"app": ["Appitizer:", "Quesadilla"],
+		"mname": ["Main Meal:", "Ham"],
+		"side": ["Side Dishes:", "Macaroni and cheese, Salad"],
+		"date": ["Date Updated:", "2013-22-04"],
+		"amofpeople": ["Amount of People:", "30"],
+		"notes": ["Notes:", "Buy expensive fireworks."]
+	},
+	"checklist17": {
+		"holigroup": ["Holiday:", "St.Patricks"],
+		"app": ["Appitizer:", "Beer"],
+		"mname": ["Main Meal:", "Hotdogs"],
+		"side": ["Side Dishes:", "Mashed Potatoes, Salad"],
+		"date": ["Date Updated:", "2013-20-04"],
+		"amofpeople": ["Amount of People:", "16"],
+		"notes": ["Notes:", "Yea."]
+	},
+	"checklist18": {
+		"holigroup": ["Holiday:", "Christmas"],
+		"app": ["Appitizer:", "Bread"],
+		"mname": ["Main Meal:", "Ham/Turkey"],
+		"side": ["Side Dishes:", "Mashed Potatoes"],
+		"date": ["Date Updated:", "2010-25-12"],
+		"amofpeople": ["Amount of People:", "16"],
+		"notes": ["Notes:", "Family Gathering."]
+	},
+	"checklist19": {
+		"holigroup": ["Holiday:", "4th Of July"],
+		"app": ["Appitizer:", "Chips"],
+		"mname": ["Main Meal:", "Ham"],
+		"side": ["Side Dishes:", "Salad"],
+		"date": ["Date Updated:", "2012-06-04"],
+		"amofpeople": ["Amount of People:", "12"],
+		"notes": ["Notes:", "Don't forget Salsa."]
+	},
+	"checklist20": {
+		"holigroup": ["Holiday:", "St.Patricks"],
+		"app": ["Appitizer:", ""],
+		"mname": ["Main Meal:", "Turkey"],
+		"side": ["Side Dishes:", "Macaroni and cheese, Salad"],
+		"date": ["Date Updated:", "2011-12-04"],
+		"amofpeople": ["Amount of People:", "22"],
+		"notes": ["Notes:", "Can't wait."]
+	},
+	
+}
